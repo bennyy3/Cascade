93c4ebfae218a64443f78d8c4edb8eb40abeacb9
bennyy3 <78427672+bennyy3@users.noreply.github.com>
2021-05-09 20:56:03 -0500

    Progress On View
    I made progress on the view class

--- Cascade/src/cascade/CascadeView.java
+++ Cascade/src/cascade/CascadeView.java
@@ -1,7 +1,11 @@
 package cascade;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
@@ -12,9 +16,30 @@ import javafx.stage.Stage;
 
 
 public class CascadeView extends Application {
+	
+	private CascadeModel myModel;
+	
+	private Label label;
+	
+	private ComboBox <Integer> combo;
+	
+	private GridPane grid;
+	
+	private Square previewSquare;
+	
+	private Button[][] buttonGrid;
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			for(int row = 0; row < 9; row++) {
+				for(int col = 0; col < 9; col++) {
+					
+				}
+			}
+			
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			
