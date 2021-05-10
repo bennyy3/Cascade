8d7c56e169b34536680de8f03e7f3c1a2cb75f83
bennyy3 <78427672+bennyy3@users.noreply.github.com>
2021-05-09 21:58:49 -0500

    View Class
    I set up the basics of the view. I created the attributes and the start function. I also added handle() propertyChange(), change(), and setGrid()

--- Cascade/src/cascade/CascadeView.java
+++ Cascade/src/cascade/CascadeView.java
@@ -1,5 +1,12 @@
 package cascade;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -15,13 +22,13 @@ import javafx.stage.Stage;
 
 
 
-public class CascadeView extends Application {
+public class CascadeView extends Application implements PropertyChangeListener, ChangeListener<String>, EventHandler<ActionEvent> {
 	
 	private CascadeModel myModel;
 	
 	private Label label;
 	
-	private ComboBox <Integer> combo;
+	private ComboBox<Integer> combo;
 	
 	private GridPane grid;
 	
@@ -34,14 +41,35 @@ public class CascadeView extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			for(int row = 0; row < 9; row++) {
-				for(int col = 0; col < 9; col++) {
-					
-				}
-			}
-			
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			root.setPrefSize(400, 400);
+			
+			combo = new ComboBox<Integer>();
+			for(int i = 1; i < 10; i++) { //initializing ComboBox of integers 1-10
+				combo.getItems().add(i);
+			}
+			combo.getSelectionModel().select(2); //set default side to 3x3
+			combo.setOnAction(this); //handle method setup
+			
+			label = new Label();
+			//TODO label.setText(myModel.getFeedback());
+			grid = new GridPane();
+			buttonGrid = new Button[10][10]; //10x10 will be max size of grid
+			for(int row = 0; row < 9; row++) {
+				for(int col = 0; col < 9; col++) {
+					buttonGrid[row][col] = new Button();
+					buttonGrid[row][col].textProperty().addListener(this);
+					buttonGrid[row][col].setPrefSize(100, 100);
+				}
+			}
+			setGrid(3);
+			Button clearButton = new Button("Clear!");
+			clearButton.setOnAction(this);
+			root.setBottom(clearButton);
+			root.setLeft(combo);
+			root.setCenter(grid);
+			root.setTop(label);
 			
 			
 			primaryStage.setScene(scene);
@@ -52,5 +80,34 @@ public class CascadeView extends Application {
 	}
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * this will get called when the model tells the view to change size
+	 * @param size: the new size of the grid to be set
+	 */
+	private void setGrid(int size) {
+		grid.getChildren().clear(); //different from model clear
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				grid.add(buttonGrid[i][j], j, i);
+			}
+		}
 	}
 }
--- Cascade/src/module-info.java
+++ Cascade/src/module-info.java
@@ -1,5 +1,7 @@
 module cascade {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires java.desktop;
+	requires javafx.base;
 	opens cascade to javafx.graphics;
 }
